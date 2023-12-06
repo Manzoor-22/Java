ba0f0ceb3726a97027e356770941de96f01dd02c
Manzoor Mohd <manzoormohd.022@gmail.com>
2023-12-06 20:48:08 +0530

    6-12

--- 11. Bitwise Operators/XOR.java
+++ 11. Bitwise Operators/XOR.java
@@ -6,9 +6,9 @@ public class XOR {
             1 ^ 0 = 1
             1 ^ 1 = 0
          */
-        int a = 7, b = 4;
+        int a = 4, b = 4;
         // a = 111
         // b = 100
-        System.out.println(a ^ b);
+        System.out.println(a ^ b  ^ 5);
     }
 }
