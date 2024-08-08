28c7e3bc0174c7510ea51ce4bb2fafc1c95d2337
Manzoor <manzoormohd.022@gmail.com>
2024-08-09 00:46:30 +0530

    9-8

---
+++ 08. 2D Arrays/SetMatZeros.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMatZeros {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+
+        setZeroes(matrix);
+    }
+    public static void setZeroes(int[][] matrix) {
+        Set<Integer> rs = new HashSet<>();
+        Set<Integer> cs = new HashSet<>();
+        int m = matrix.length, n = matrix[0].length;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    rs.add(i);
+                    cs.add(j);
+                }
+            }
+        }
+
+        System.out.println(rs);
+        System.out.println(cs);
+    }
+}
