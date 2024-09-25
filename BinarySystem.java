0f0938098e0fcaec912017f2b7ff30c02d91a35a
Manzoor <Manzoormohd.022@gmail.com>
2024-09-25 15:52:43 +0530

    25-09

---
+++ BinarySystem.java
@@ -0,0 +1,15 @@
+public class BinarySystem {
+    public static void main(String[] args) {
+        String str = "111";
+        int pow = 1;
+        int ans = 0;
+
+        for(int i = str.length()-1; i >= 0; i--){
+            int digit = Character.getNumericValue(str.charAt(i));
+            ans = ans + pow*digit;
+            pow *= 2;
+        }
+
+        System.out.println(ans);
+    }
+}
